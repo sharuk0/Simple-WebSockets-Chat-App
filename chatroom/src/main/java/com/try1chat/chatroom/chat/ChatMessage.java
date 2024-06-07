cc879462d17ad2c2729948fcbdf3379372aa631b
Sharuk0 <144263475+sharuk0@users.noreply.github.com>
2024-06-07 18:40:09 -0500

    Add files via upload

---
+++ chatroom/src/main/java/com/try1chat/chatroom/chat/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.try1chat.chatroom.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+}
---
+++ chatroom/src/main/java/com/try1chat/chatroom/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.try1chat.chatroom.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
